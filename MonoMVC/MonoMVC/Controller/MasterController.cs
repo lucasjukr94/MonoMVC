b98820972471c3ccc674dcd8d5654133f72a26c1
Lucas Ju <lucasjukr94@gmail.com>
2019-07-02 10:38:09 -0300

    Cursor

--- MonoMVC/MonoMVC/Controller/MasterController.cs
+++ MonoMVC/MonoMVC/Controller/MasterController.cs
@@ -6,32 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoMVC.Model.Entity;
 
 namespace MonoMVC.Controller
 {
     public class MasterController : Controller
     {
-        private Texture2D texture;
-        private Rectangle rectangle;
+        private Cursor cursor;
+        private string text;
 
         public override void Load()
         {
-            rectangle = new Rectangle(0, 0, 100, 50);
-            texture = new Texture2D(Global.GraphicsDevice, rectangle.Width, rectangle.Height);
-            Color[] color = new Color[rectangle.Width*rectangle.Height];
-            for (int i = 0; i < color.Length; ++i) color[i] = Color.Black;
-            texture.SetData(color);
+            cursor = new Cursor();
         }
 
         public override void Update(GameTime gameTime)
         {
-            
+            cursor.Move();
+            text = string.Format("X:{0}, Y:{1}", cursor.Vector2.X, cursor.Vector2.Y);
         }
 
         public override void Render()
         {
-            //Global.GraphicsDevice.Clear(Color.Green);
-            Global.SpriteBatch.Draw(texture, new Vector2(rectangle.X, rectangle.Y));
+            Global.SpriteBatch.Draw(cursor.Texture2D, cursor.Vector2);
+            Global.SpriteBatch.DrawString(Global.SpriteFont, text, Vector2.Zero, Color.Black);
         }
     }
 }
---
+++ MonoMVC/MonoMVC/Model/Entity/Cursor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoMVC.Model.Entity
+{
+    public class Cursor : Entity
+    {
+        public Cursor()
+        {
+            Rectangle = new Rectangle(0, 0, 20, 20);
+            Texture2D = RectangleTexture2D;
+        }
+
+        public void Move()
+        {
+            Rectangle = new Rectangle(Mouse.GetState().X, Mouse.GetState().Y, Rectangle.Width, Rectangle.Height);
+        }
+    }
+}
